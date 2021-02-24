3b1cbaee9dbabce34b5d3c8612ca77c0190a8701
codeclanstudent <sstewart1993@outlook.com>
2021-02-24 13:26:51 +0000

    location

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." />
       </div> 
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northhampton, UK</div>
+          <div class="date"></div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -43,6 +49,35 @@ main{
   height: 100vh;
   padding:25px;
   background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+background-color: rgba(255, 255, 255, 0.75);
+border-radius: 16px 0px 16px 0px;
 }
 
 </style>
